000637ca7e638c4224660509fcfc343a3a89d94f
Michael Nguyen <92574795+Blossommn@users.noreply.github.com>
2023-12-29 15:12:29 -0700

    Update main.py
    Added try/exception error code

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,15 +1,19 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format:
 letters = pandas.read_csv("nato_phonetic_alphabet.csv")
-
-# for (index, row) in letters.iterrows():
-#     print(row.letter)
 
 letters_dict = {row.letter: row.code for (index, row) in letters.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word_input = input("Type in a word to translate: ").upper()
 
-phonetic_word = [letters_dict[letter] for letter in word_input]
-print(phonetic_word)
+def generate_phonetic():
+    word_input = input("Type in a word to translate: ").upper()
+    try:
+        phonetic_word = [letters_dict[letter] for letter in word_input]
+    except KeyError as error_message:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_word)
+
+
+generate_phonetic()
